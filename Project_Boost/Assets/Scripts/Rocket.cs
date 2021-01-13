0e3a360da11ec1cc0afa14e1c7c163f2bf2ad1df
Anton Astanin <hidan_jamanako@hotmail.com>
2021-01-13 23:34:09 +0300

    engine sound

--- Project_Boost/Assets/Scripts/Rocket.cs
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rotatingForce = 1f;
+    [SerializeField] float thrustingForce = 1f;
+    [SerializeField] float thrustingVolume = 0.8f;
+    [SerializeField] float thrustingPitch = 1.6f;
 
     Rigidbody myRigidbody;
+    AudioSource myAudioSource;
+    float defaultAudioSourcePitch = 0f;
+    float defaultAudioSourceVolume = 0f;
 
     void Start()
     {
         myRigidbody = this.GetComponent<Rigidbody>();
+        myAudioSource = this.GetComponent<AudioSource>();
+        defaultAudioSourcePitch = myAudioSource.pitch;
+        defaultAudioSourceVolume = myAudioSource.volume;
     }
 
     void Update()
@@ -23,7 +32,12 @@ public class Rocket : MonoBehaviour
         // Thrust 'Space'
         if (Input.GetKey(KeyCode.Space))
         {
-            myRigidbody.AddRelativeForce(Vector3.up);
+            myRigidbody.AddRelativeForce(Vector3.up * thrustingForce);
+            SetThrustingSound();
+        }
+        else
+        {
+            SetIdleSound();
         }
 
         // Rotate 'wAsD'
@@ -34,6 +48,24 @@ public class Rocket : MonoBehaviour
         else if (Input.GetKey(KeyCode.D))
         {
             this.transform.Rotate(-Vector3.forward * Time.deltaTime * rotatingForce);
+        }
+    }
+
+    void SetThrustingSound()
+    {
+        if (myAudioSource)
+        {
+            myAudioSource.pitch = thrustingPitch;
+            myAudioSource.volume = thrustingVolume;
+        }
+    }
+
+    void SetIdleSound()
+    {
+        if (myAudioSource)
+        {
+            myAudioSource.pitch = defaultAudioSourcePitch;
+            myAudioSource.volume = defaultAudioSourceVolume;
         }
     }
 }
